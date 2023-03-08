244e5546446fd8a8c85f3e944a026e65b744aecb
Pouya Vedadiyan <vedadiyan@gmail.com>
2023-03-08 12:42:19 +0800

    http improvements

--- pkg/http/url.go
+++ pkg/http/url.go
@@ -4,44 +4,39 @@ import (
 	"bytes"
 	"fmt"
 	"net/url"
+	"strings"
 )
 
-type Query map[string]string
+type Query map[string][]string
+type RouteValues map[string]string
 
 type IUrl interface {
 	Url() (*url.URL, error)
 }
 
-type urlBuilder struct {
-	relativeUrl string
-	query       Query
+type UrlTemplate struct {
+	template string
 }
 
-func (urlBuilder urlBuilder) Url() (*url.URL, error) {
-	var _bytes []byte
-	buffer := bytes.NewBufferString(urlBuilder.relativeUrl)
-	if urlBuilder.query != nil {
-		buffer.WriteString("?")
-		for key, value := range urlBuilder.query {
-			buffer.WriteString(fmt.Sprintf("%s=%s", key, url.QueryEscape(value)))
-			buffer.WriteString("&")
-		}
-		_bytes = buffer.Bytes()
-		_bytes = _bytes[0 : len(_bytes)-1]
-	} else {
-		_bytes = buffer.Bytes()
+func NewUrlTemplate(template string) *UrlTemplate {
+	urlTemplate := UrlTemplate{
+		template: template,
 	}
-	return url.Parse(string(_bytes))
+	return &urlTemplate
 }
 
-func Url(url string, query Query) *urlBuilder {
-	return &urlBuilder{url, query}
-}
-
-func BaseUrl(baseUrl string) *urlBuilder {
-	return &urlBuilder{baseUrl, nil}
-}
-
-func ExtendUrl(baseUrl *url.URL, relativeUrl string, query Query) *urlBuilder {
-	return &urlBuilder{fmt.Sprintf("%s://%s/%s", baseUrl.Scheme, baseUrl.Host, relativeUrl), query}
+func (urlTemplate UrlTemplate) Url(routeValues RouteValues, query Query) (*url.URL, error) {
+	copy := urlTemplate.template
+	buffer := bytes.NewBufferString("?")
+	for key, value := range routeValues {
+		copy = strings.ReplaceAll(copy, key, value)
+	}
+	for key, values := range query {
+		for _, value := range values {
+			buffer.WriteString(fmt.Sprintf("%s=%s&", key, value))
+		}
+	}
+	bytes := buffer.Bytes()
+	finalUrl := fmt.Sprintf("%s%s", copy, string(bytes[:len(bytes)-1]))
+	return url.Parse(finalUrl)
 }
