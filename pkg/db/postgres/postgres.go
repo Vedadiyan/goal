ee6ddef93b248265fad4c43cb0a01a8f9027a625
Pouya Vedadiyan <vedadiyan@gmail.com>
2024-01-23 18:30:23 +0800

    postgres fix

--- pkg/db/postgres/postgres.go
+++ pkg/db/postgres/postgres.go
@@ -28,11 +28,13 @@ type Pool struct {
 func getPgxSql(sql string, arguments map[string]any) (string, []any) {
 	_sql := sql
 	index := 0
-	_arguments := make([]any, len(arguments))
+	_arguments := make([]any, 0)
 	for key, value := range arguments {
-		_sql = strings.ReplaceAll(_sql, fmt.Sprintf("\"%s\"", key), fmt.Sprintf("$%d", index+1))
-		_arguments[index] = value
-		index++
+		if strings.Contains(_sql, fmt.Sprintf("\"%s\"", key)) {
+			_sql = strings.ReplaceAll(_sql, fmt.Sprintf("\"%s\"", key), fmt.Sprintf("$%d", index+1))
+			_arguments = append(_arguments, value)
+			index++
+		}
 	}
 	return _sql, _arguments
 }
